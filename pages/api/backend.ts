71cd504b10ef088b9b39b14147aed1cf46839825
woddyowl <khakho.morad@gmail.com>
2022-11-16 19:42:19 -0800

    env fix 2

--- pages/api/backend.ts
+++ pages/api/backend.ts
@@ -1,7 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+// Next.js API route support: https://nextjs.org/docs/api-routes/introductions
 import { Configuration, OpenAIApi } from "openai";
-console.log(process.env.OPENAI_API_KEY)
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 
@@ -9,10 +7,11 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function handler(
-  reqL:any,
+  req:any,
   res:any
 ) {
   console.log("clicked")
+  try{
   const promp_request = `write an essay about turtles`;
   
   
@@ -22,9 +21,13 @@ export default async function handler(
     temperature: 0.6,
     max_tokens: 1000,
   });
-  const respons = await completion.data.choices[0].text;
-  
-  
- 
+  console.log(completion)
+  const respons = completion.data.choices[0].text;
   res.status(200).json({ data: respons })
+}catch(error){
+  res.status(500)
+  console.log(error)
+}
+ 
+
 }
